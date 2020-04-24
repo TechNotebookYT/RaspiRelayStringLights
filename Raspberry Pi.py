1599617c889b940bc1ae87ef9b2288238db0d649
Tech Notebook <technotebook@yahoo.com>
2020-04-24 14:51:48 -0500

    Add files via upload

---
+++ Raspberry Pi.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(26, GPIO.OUT)
+
+
+
+try:
+    counter = -1
+    while True:
+        counter += 1
+        if counter % 2 == 0:
+            GPIO.output(26, GPIO.HIGH)
+        elif counter % 2 != 0:
+            GPIO.output(26, GPIO.LOW)
+        else:
+            raise Exception("Boolean Error")
+        input("Press Enter to Toggle")
+        
+finally:
+    GPIO.cleanup()
